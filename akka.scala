ad5cdb5fe73427f1ada6ed1377c6a35cf97b7afa
Reynold <orzreynold@gmail.com>
2014-06-15 19:45:22 +0800

    week2 practice

--- akka.scala
+++ akka.scala
@@ -1,11 +1,12 @@
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
+import scala.util.Random
 
-
-class HelloActor extends Actor {
+class HelloActor extends Actor with IntChange{
   def receive = {
-    case "hello" => println("hello back at you")
+    case n:Int => mPrint(n)
+    case "lottery" => randomPrint()
     case _       => println("huh?")
   }
 }
@@ -14,8 +15,23 @@ object Main extends App {
   val system = ActorSystem("HelloSystem")
   // default Actor constructor
   val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
-  helloActor ! "hello"
-  helloActor ! "buenos dias"
- 
+  helloActor ! 1
+  helloActor ! 2
+  helloActor ! 3
+  helloActor ! 4
+  helloActor ! 5
+  helloActor ! "lottery"
 }
 
+trait IntChange {
+val x = Map(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five")
+def mPrint(n:Int){
+println(x(n))
+}
+def randomPrint(){
+for(i<-0 to 5){
+val r1 = Random.nextInt(89) + 10
+println(r1)
+}
+}
+}
