fe80b8770a2f58a3b17f490248dfc1b231000f1c
rekiwu <rekiwu@gmail.com>
2014-06-15 19:42:38 +0800

    week2 HW

--- week2/week2.scala
+++ week2/week2.scala
@@ -1,12 +1,29 @@
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
+import scala.util.Random
+
+trait random {
+
+  def randomInt {
+    for (i <- 0 to 6) {
+	var random : Int = Random.nextInt(100)
+        if (random < 10) random = random*10
+        print (random +" ")
+    }
+  }  
+}
 
 
-class HelloActor extends Actor {
+class HelloActor extends Actor with random{
+
+  val intMapeng = Map(1 -> "one", 2 -> "two", 3 -> "three", 4->"four", 5->"five")
+	
   def receive = {
-    case "2" => println("two")
-    case "lottery"  => println("34" + "56")
+
+    case n:Int => println(intMapeng(n))
+    
+    case "lottery"  => randomInt
   }
 }
 
@@ -15,7 +32,7 @@ object Main extends App {
   // default Actor constructor
 
   val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
-  helloActor ! "2"
+  helloActor ! 2
   helloActor ! "lottery"
  
 }
